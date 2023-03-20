d92cacb9633b04843e9d282e64f646bf3fa26971
omegamaster86 <masya868686@gmail.com>
2023-03-20 16:59:01 +0900

    クリア

--- 040_変数/910_修了確認_falsyとtruthy/start/main.js
+++ 040_変数/910_修了確認_falsyとtruthy/start/main.js
@@ -2,15 +2,15 @@
  * 問題１：
  * 以下のコンソールにはどのような値が表示されるでしょうか？
  */
-// console.log("0 == false", 0 == false);
-// console.log("0 === false", 0 === false);
-// console.log('"false" == false', "false" == false);
-// console.log('"0" == 0', "0" == 0);
-// console.log('Boolean("0") === false', Boolean("0") === false);
-// console.log('Boolean(0) === false', Boolean(0) === false);
-// console.log('!Boolean(0) === false', !Boolean(0) === false);
-// console.log('-1 == false', -1 == false);
-// console.log('!10 === false', !10 === false);
+console.log("0 == false", 0 == false);
+console.log("0 === false", 0 === false);
+console.log('"false" == false', "false" == false);
+console.log('"0" == 0', "0" == 0);
+console.log('Boolean("0") === false', Boolean("0") === false);
+console.log('Boolean(0) === false', Boolean(0) === false);
+console.log('!Boolean(0) === false', !Boolean(0) === false);
+console.log('-1 == false', -1 == false);
+console.log('!10 === false', !10 === false);
 
 /**
  * 問題２：
@@ -23,7 +23,9 @@
 let a = 1;
 
 function fn(num) {
-    num = num || -1;
+    if (num === undefined || num === null) {
+    num = -1;
+    }
     console.log(num);
 }
 fn(a);
@@ -39,3 +41,11 @@ fn(a);
  * greeting("Bob"); -> 出力結果："hello, Bob"
  *
  */
+
+function greeting(name, hi) {
+    hi = hi || 'hello';
+    console.log(`${hi},${name}`);
+}
+
+greeting("Bob","hi");
+greeting("Bob");
