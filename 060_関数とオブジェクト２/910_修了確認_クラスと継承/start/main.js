349f2f38971a380c882a2981389c0cce3ff4230f
omegamaster86 <masya868686@gmail.com>
2023-03-29 19:49:15 +0900

    クリア

--- 060_関数とオブジェクト２/910_修了確認_クラスと継承/start/main.js
+++ 060_関数とオブジェクト２/910_修了確認_クラスと継承/start/main.js
@@ -43,6 +43,41 @@
  * login failed <- loginで失敗した場合
  */
 
+class User {
+  constructor(name) {
+    this.name = name;
+    this.redirectTo = "/";
+  }
+
+  login() {
+    console.log(`User:${this.name}`);
+    return true;
+  }
+
+  checkRoll() {
+    console.log("you have normal roll");
+    return true;
+  }
+
+  redirect() {
+    console.log(`redirect: ${this.redirectTo}`)
+    return true
+  }
+}
+
+class AdminUser extends User {
+  constructor(name){
+    super(name);
+    this.redirectTo = "/admin";
+  }
+
+  checkRoll() {
+    console.log("you have admin roll");
+    return true;
+  }
+
+}
+
 function loginController(user) {
   if (user.login()
     && user.checkRoll()
@@ -52,3 +87,6 @@ function loginController(user) {
     console.log('login failed');
   }
 }
+
+loginController(new User("Bob"));
+loginController(new AdminUser("Bob"));
